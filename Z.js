98eb77be8878753d82b48044cf28f0e9536769c2
ifurquan <149615243+ifurquan@users.noreply.github.com>
2024-01-05 11:28:35 -0600

    update

--- Z.js
+++ Z.js
@@ -1,2 +1,50 @@
 let y = "Hello world";
 console.log(y);
+
+y = "Hello IC Stars";
+console.log(y);
+
+console.log(y === "Hello IC Stars");
+
+let x;
+
+document.addEventListener("click", (e) => {
+  console.log("You have clicked anywhere in the document.");
+});
+
+const grabFirstnameInput = document.getElementById("firstNameInput");
+console.log(grabFirstnameInput);
+
+const grabBirthPlaceInput = document.getElementById("birthPlaceInput");
+console.log(grabBirthPlaceInput);
+
+const grabColorInput = document.getElementById("color");
+console.log(grabColorInput);
+
+const grabSubmitBtn = document.getElementById("submitBtn");
+console.log(grabSubmitBtn);
+
+grabSubmitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("this is the LIKE button");
+  console.log(grabFirstnameInput.value);
+  console.log(grabBirthPlaceInput.value);
+  console.log(grabColorInput.value);
+
+  const paragraphElement = document.querySelector(".paragraph");
+  console.log(paragraphElement);
+
+  paragraphElement.textContent = `${grabFirstnameInput.value} born in ${grabBirthPlaceInput.value} your favortie color is ${grabColorInput.value}. Thank you for liking me.`;
+
+  // When we click the like button the paragraph will appear*//
+  //Isaiah Furquan, your birthday is 02/07/1994. Thank you for liking me.*//
+
+  // console.log(`${} ${} your birthday is ${}. Thank you for liking me.`)
+
+  console.log(
+    `${grabFirstnameInput.value} ${grabLastnameInput.value} your favorite color is ${grabColorInput.value}. Thank you for liking me.`
+  );
+
+  const grabFirstnameInput = document.getElementById("firstNameLabel");
+  grabFirstnameInput.style.color = "red";
+});
